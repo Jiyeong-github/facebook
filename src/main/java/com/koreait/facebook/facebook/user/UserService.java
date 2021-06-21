c8e01461a591a0a99c6457a3048021d1418eb4bf
Jiyeong-github <81175088+Jiyeong-github@users.noreply.github.com>
2021-06-21 15:04:56 +0900

    Initial commit

---
+++ src/main/java/com/koreait/facebook/facebook/user/UserService.java
@@ -0,0 +1,12 @@
+package com.koreait.facebook.facebook.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserMapper mapper;
+
+
+}
